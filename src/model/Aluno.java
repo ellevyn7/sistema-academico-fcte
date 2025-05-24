b0d47b8b88dce4a1fe8bf2ab71ee7a24d16044c1
ellevyn7 <evellynrocha8524@gmail.com>
2025-05-24 16:54:27 -0300

    Adiciona classes Aluno e AlunoNormal

---
+++ src/model/Aluno.java
@@ -0,0 +1,52 @@
+
+package model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Aluno implements Serializable {
+    protected String nome;
+    protected String matricula;
+    protected String curso;
+    protected List<Disciplina> disciplinasMatriculadas;
+
+    public Aluno(String nome, String matricula, String curso) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.curso = curso;
+        this.disciplinasMatriculadas = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public List<Disciplina> getDisciplinasMatriculadas() {
+        return disciplinasMatriculadas;
+    }
+
+    public void matricularDisciplina(Disciplina disciplina) {
+        disciplinasMatriculadas.add(disciplina);
+    }
+
+    public abstract boolean podeMatricularMais();
+
+    @Override
+    public String toString() {
+        return "Aluno{" +
+                "nome='" + nome + '\'' +
+                ", matricula='" + matricula + '\'' +
+                ", curso='" + curso + '\'' +
+                ", disciplinasMatriculadas=" + disciplinasMatriculadas.size() +
+                '}';
+    }
+}
